d6155e07d4517509188799dca0aec90479385b3e
Felipe Navas <felipemancianavs@gmail.com>
2026-03-12 02:54:34 -0400

    Add initial Swagger configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './config/swagger.js';
+
 import errorHandler from './middleware/errorHandler.js';
 import dbRoutes from './routes/dbRoutes.js';
 import recipeRoutes from './routes/recipeRoutes.js';
@@ -8,12 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get('/', (_req, res) => {
     res.json({ message: 'Whisk API is running' });
 });
 
 app.use('/db', dbRoutes)
 app.use('/recipe', recipeRoutes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use(errorHandler);
 
--- backend/src/routes/dbRoutes.js
+++ backend/src/routes/dbRoutes.js
@@ -3,6 +3,27 @@ import { getDBHealth } from "../controllers/dbController.js";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /db/health:
+ *   get:
+ *     summary: Check database health
+ *     tags:
+ *       - Database
+ *     responses:
+ *       200:
+ *         description: Database is connected
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 db:
+ *                   type: string
+ *                   example: connected
+ *       500:
+ *         description: Database connection failed
+ */
 router.get("/health", getDBHealth);
 
 export default router;
