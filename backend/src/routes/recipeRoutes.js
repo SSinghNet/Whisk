b5e332475892732492a79a8fa6c63a9dfd347e2c
matthewmatro <mjm857@scarletmail.rutgers.edu>
2026-03-31 13:12:52 -0400

    fix merge conflicts

--- backend/src/routes/recipeRoutes.js
+++ backend/src/routes/recipeRoutes.js
@@ -1,9 +1,31 @@
 import { Router } from "express";
-import { getRecipes, getRecipe } from "../controllers/recipeController.js";
+import {
+  getRecipes,
+  getRecipe,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe
+} from "../controllers/recipeController.js";
 
 const router = Router();
 
+// GET /recipes
 router.get('/', getRecipes);
+
+// GET /recipes/:id
 router.get('/:id', getRecipe);
 
+<<<<<<< HEAD
 export default router;
+=======
+// POST /recipes
+router.post('/', createRecipe);
+
+// PUT /recipes/:id
+router.put('/:id', updateRecipe);
+
+// DELETE /recipes/:id
+router.delete('/:id', deleteRecipe);
+
+export default router;
+>>>>>>> dd4ffd9 (recipe endpoints)
